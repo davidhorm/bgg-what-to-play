c4e4309ac5da29009ca3b0100ba4d5756021c0cd
David Horm <davidhorm@gmail.com>
2023-01-08 19:41:53 -0800

    Add @testing-library and test <SearchFilterForm />

--- src/components/SearchFilterForm/SearchFilterForm.spec.tsx
+++ src/components/SearchFilterForm/SearchFilterForm.spec.tsx
@@ -1,6 +1,39 @@
-import { describe, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { beforeEach, describe, expect, test } from "vitest";
 import { SearchFilterForm } from "./SearchFilterForm";
 
+const ORIGINAL_LOCATION = location.href;
+
 describe(SearchFilterForm.name, () => {
-  test.todo("");
+  beforeEach(() => {
+    // clear out the query parameters between each test
+    history.replaceState({}, "", ORIGINAL_LOCATION);
+
+    // clear body between each render
+    document.body.innerHTML = "";
+  });
+
+  test("WHEN submit `davidhorm` as username, THEN query params is `?username=davidhorm`", async () => {
+    const user = userEvent.setup();
+    render(<SearchFilterForm />);
+    const input: HTMLInputElement = screen.getByLabelText("BGG Username");
+
+    expect(location.search).toBe("");
+    expect(input.value).toBe("");
+
+    await user.type(input, "davidhorm{enter}");
+
+    expect(input.value).toBe("davidhorm");
+    expect(location.search).toBe("?username=davidhorm");
+  });
+
+  test("WHEN location already has username query param, THEN input box is populated with the same value", async () => {
+    history.replaceState({}, "", `${ORIGINAL_LOCATION}?username=davidhorm`);
+    render(<SearchFilterForm />);
+    const input = screen.getByLabelText("BGG Username") as HTMLInputElement;
+
+    expect(location.search).toBe("?username=davidhorm");
+    expect(input.value).toBe("davidhorm");
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+/**
+ * @see https://vitejs.dev/config/
+ * @see https://vitest.dev/config/
+ */
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
 });
