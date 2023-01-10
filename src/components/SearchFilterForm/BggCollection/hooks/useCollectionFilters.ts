c4361cda20fc5b03157725d04f6735ddf77d69bb
David Horm <davidhorm@gmail.com>
2023-01-10 08:01:51 -0800

    Refactor Reducer - based off of https://stackoverflow.com/questions/74884329/how-to-derive-action-type-from-mapping-object-for-usereducer-dispatch-type-safet

--- src/components/SearchFilterForm/BggCollection/hooks/useCollectionFilters.ts
+++ src/components/SearchFilterForm/BggCollection/hooks/useCollectionFilters.ts
@@ -8,33 +8,59 @@ const initialState = {
 
 export type CollectionFilters = typeof initialState;
 
-// TODO: make action types easier to manage (p3)
-type Action =
-  | { type: "RESET_FILTERS" }
-  | { type: "TOGGLE_SHOW_INVALID_PLAYER_COUNT" }
-  | { type: "TOGGLE_FILTER_BY_PLAYER_COUNT_ACTIVE" }
-  | { type: "SET_FILTER_BY_PLAYER_COUNT_VALUE"; payload: number };
-
-const reducer = (
+type ActionHandler<T> = (
   state: CollectionFilters,
-  action: Action
-): CollectionFilters => {
-  if (action.type === "RESET_FILTERS") return initialState;
+  payload: T
+) => CollectionFilters;
 
-  if (action.type === "TOGGLE_SHOW_INVALID_PLAYER_COUNT")
-    return { ...state, showInvalidPlayerCount: !state.showInvalidPlayerCount };
+const resetFilters: ActionHandler<Partial<undefined>> = () => initialState;
 
-  if (action.type === "TOGGLE_FILTER_BY_PLAYER_COUNT_ACTIVE")
-    return {
-      ...state,
-      filterByPlayerCountActive: !state.filterByPlayerCountActive,
-    };
+const toggleShowInvalidPlayerCount: ActionHandler<Partial<undefined>> = (
+  state
+) => ({ ...state, showInvalidPlayerCount: !state.showInvalidPlayerCount });
 
-  if (action.type === "SET_FILTER_BY_PLAYER_COUNT_VALUE")
-    return { ...state, filterByPlayerCountValue: action.payload };
+const toggleFilterByPlayerCountActive: ActionHandler<Partial<undefined>> = (
+  state
+) => ({
+  ...state,
+  filterByPlayerCountActive: !state.filterByPlayerCountActive,
+});
 
-  return state;
+const setFilterByPlayerCountValue: ActionHandler<number> = (
+  state,
+  payload
+) => ({ ...state, filterByPlayerCountValue: payload });
+
+const actions = {
+  RESET_FILTERS: resetFilters,
+  TOGGLE_SHOW_INVALID_PLAYER_COUNT: toggleShowInvalidPlayerCount,
+  TOGGLE_FILTER_BY_PLAYER_COUNT_ACTIVE: toggleFilterByPlayerCountActive,
+  SET_FILTER_BY_PLAYER_COUNT_VALUE: setFilterByPlayerCountValue,
 };
+
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+type Payload<T> = T extends (...args: any[]) => any ? Parameters<T>[1] : never;
+
+type CreateActions<T extends typeof actions> = {
+  [K in keyof T]: Payload<T[K]> extends undefined
+    ? {
+        type: K;
+        payload?: undefined;
+      }
+    : {
+        type: K;
+        payload: Payload<T[K]>;
+      };
+}[keyof T];
+
+type Action = CreateActions<typeof actions>;
+
+/** Reducer pattern based off of https://stackoverflow.com/questions/74884329/how-to-derive-action-type-from-mapping-object-for-usereducer-dispatch-type-safet */
+const reducer: ActionHandler<Action> = (state, action): CollectionFilters =>
+  (actions[action.type] as ActionHandler<typeof action["payload"]>)(
+    state,
+    action.payload
+  );
 
 export const useCollectionFilters = () => {
   const [filterState, filterDispatch] = useReducer(reducer, initialState);
