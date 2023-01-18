69ef62a47068dfb49ecfb4610ea60798cb1e6f65
David Horm <davidhorm@gmail.com>
2023-01-17 17:54:53 -0800

    Misc fixes to Slider - Fix getAriaLabel with proper label - filterDispatch onChangeCommitted so don't need to debounce - local state for value and onChange

--- src/components/SearchFilterForm/PlayerCountSlider.tsx
+++ src/components/SearchFilterForm/PlayerCountSlider.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import type { CollectionFilterReducer } from "@/types";
 import Slider from "@mui/material/Slider";
 
 type Props = {
   filter: CollectionFilterReducer;
 };
+
+const getAriaLabel = (index: number) =>
+  index === 0 ? "Minimum Player Count" : "Maximum Player Count";
 
 const getValueLabel = (value: number) =>
   value > 10 ? "10+" : value.toString();
@@ -18,29 +22,33 @@ const convertElevenToInfinity = (value: number) =>
 
 export const PlayerCountSlider = ({
   filter: { filterState, filterDispatch },
-}: Props) => (
-  <div className="mt-2 mr-6 flex flex-col">
-    <label htmlFor="player-count" className="text-sm">
-      Filter by Player Count
-    </label>
-    <Slider
-      id="player-count"
-      className="mx-4 mt-8"
-      getAriaLabel={getValueLabel}
-      getAriaValueText={getValueLabel}
-      valueLabelFormat={getValueLabel}
-      valueLabelDisplay="on"
-      min={1}
-      max={11}
-      marks={marks}
-      scale={convertElevenToInfinity}
-      value={filterState.playerCountRange}
-      onChange={(_, value) => {
-        filterDispatch({
-          type: "SET_PLAYER_COUNT_RANGE",
-          payload: value as [number, number],
-        });
-      }}
-    />
-  </div>
-);
+}: Props) => {
+  const [range, setRange] = useState(filterState.playerCountRange);
+
+  return (
+    <div className="mt-2 mr-6 flex flex-col">
+      <label htmlFor="player-count" className="text-sm">
+        Filter by Player Count
+      </label>
+      <Slider
+        id="player-count"
+        className="mx-4"
+        getAriaLabel={getAriaLabel}
+        getAriaValueText={getValueLabel}
+        valueLabelFormat={getValueLabel}
+        min={1}
+        max={11}
+        marks={marks}
+        scale={convertElevenToInfinity}
+        value={range}
+        onChange={(_, value) => setRange(value as [number, number])}
+        onChangeCommitted={(_, value) => {
+          filterDispatch({
+            type: "SET_PLAYER_COUNT_RANGE",
+            payload: value as [number, number],
+          });
+        }}
+      />
+    </div>
+  );
+};
