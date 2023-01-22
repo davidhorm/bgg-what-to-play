558fb22b6de4a4926a0ba162ab2fe2cc5825f610
David Horm <davidhorm@gmail.com>
2023-01-22 10:55:54 -0800

    Test Default Filter Control Values and Query Parameters

--- tests/filter-controls-query-params.spec.ts
+++ tests/filter-controls-query-params.spec.ts
@@ -1,14 +1,32 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Filter Controls and Query Parameters", () => {
-  test("Default Filter Control Values", async ({ page }) => {
+  test("Default Filter Control Values and Query Parameters", async ({
+    page,
+  }) => {
     await page.goto("/");
 
-    // TODO: await https://github.com/microsoft/playwright/issues/19284 to be released
-    // const x = await page.getByLabel("Minimum Player Count");
-    const minPlayerCount = await page.locator(
-      "[aria-label='Minimum Player Count']"
+    /**
+     * Placeholder fix for `getByLabel` to query by `aria-label`.
+     * TODO: replace with `page.getByLabel()` when released.
+     * @see https://github.com/microsoft/playwright/issues/19284
+     */
+    const getByLabel = (label: string) =>
+      page.locator(`[aria-label='${label}']`);
+
+    await expect(page.getByLabel("BGG Username")).toHaveValue("");
+
+    await expect(getByLabel("Minimum Player Count")).toHaveValue("1");
+
+    await expect(getByLabel("Maximum Player Count")).toHaveAttribute(
+      "aria-valuetext",
+      "10+"
     );
-    await expect(minPlayerCount).toHaveValue("1");
+
+    await expect(
+      page.getByLabel("Show invalid player counts")
+    ).not.toBeChecked();
+
+    expect(new URL(page.url()).search).toBe("");
   });
 });
