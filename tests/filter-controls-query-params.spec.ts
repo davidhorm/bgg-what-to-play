78fe299abaa9638f02d78089a3abc601a598dac4
David Horm <davidhorm@gmail.com>
2023-01-28 10:21:41 -0800

    Upgrade to latest Playwright

--- tests/filter-controls-query-params.spec.ts
+++ tests/filter-controls-query-params.spec.ts
@@ -6,19 +6,11 @@ test.describe("Filter Controls and Query Parameters", () => {
   }) => {
     await page.goto("/");
 
-    /**
-     * Placeholder fix for `getByLabel` to query by `aria-label`.
-     * TODO: replace with `page.getByLabel()` when released.
-     * @see https://github.com/microsoft/playwright/issues/19284
-     */
-    const getByLabel = (label: string) =>
-      page.locator(`[aria-label='${label}']`);
-
     await expect(page.getByLabel("BGG Username")).toHaveValue("");
 
-    await expect(getByLabel("Minimum Player Count")).toHaveValue("1");
+    await expect(page.getByLabel("Minimum Player Count")).toHaveValue("1");
 
-    await expect(getByLabel("Maximum Player Count")).toHaveAttribute(
+    await expect(page.getByLabel("Maximum Player Count")).toHaveAttribute(
       "aria-valuetext",
       "10+"
     );
