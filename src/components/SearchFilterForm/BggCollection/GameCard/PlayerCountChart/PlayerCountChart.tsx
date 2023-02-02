dcec79c39965f3f5997b22f49b8f42888e297853
David Horm <davidhorm@gmail.com>
2023-02-01 21:04:25 -0800

    Refactor PlayerCountChart into multiple files

--- src/components/SearchFilterForm/BggCollection/GameCard/PlayerCountChart/PlayerCountChart.tsx
+++ src/components/SearchFilterForm/BggCollection/GameCard/PlayerCountChart/PlayerCountChart.tsx
@@ -10,6 +10,8 @@ import {
   Tooltip,
   XAxis,
 } from "recharts";
+import { CustomTooltip } from "./CustomTooltip";
+import { MaybeNoDataAvailable } from "./MaybeNoDataAvailable";
 
 type Recommendation = "Best" | "Recommended" | "Not Recommended";
 
@@ -36,91 +38,6 @@ const getFill = (
   }
 
   return defaultColor;
-};
-
-/** If the polling data has zero votes, then display "No Data Available" text */
-const MaybeNoDataAvailable = (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  { data, height, width }: any // disable any because of how <Customized component /> works
-) => {
-  if (
-    (data as BoardGame["recommendedPlayerCount"]).filter(
-      (rec) =>
-        rec["Not Recommended"] !== 0 ||
-        rec["Recommended"] !== 0 ||
-        rec["Best"] !== 0
-    ).length > 0
-  ) {
-    return null;
-  }
-
-  return (
-    <text
-      dy={height / 2}
-      dx={width / 2}
-      fontSize="0.875rem"
-      textAnchor="middle"
-    >
-      No Recommendations Available
-    </text>
-  );
-};
-
-const CustomTooltip = (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  { payload, label }: any
-) => {
-  if (!payload[0]?.payload) return null;
-
-  const {
-    Best,
-    Recommended,
-    ["Not Recommended"]: notRecNeg,
-  } = payload[0].payload;
-  const notRec = Math.abs(notRecNeg);
-  const total = Best + Recommended + notRec;
-
-  const getPercentage = (value: number) =>
-    ` (${Math.round((value / total) * 100)}%)`;
-
-  return (
-    <div className="bg-white/90 p-1 text-left text-sm">
-      <p>Player Count: {label}</p>
-      <ul className="table list-none pl-1">
-        <li className="table-row">
-          <span className="table-cell text-right" role="img" aria-label="best">
-            😍:
-          </span>
-          <span className="table-cell px-1 text-right">{Best}</span>
-          <span className="table-cell text-right">{getPercentage(Best)}</span>
-        </li>
-        <li className="table-row">
-          <span
-            className="table-cell text-right"
-            role="img"
-            aria-label="recommended"
-          >
-            🙂:
-          </span>
-          <span className="table-cell px-1 text-right">{Recommended}</span>
-          <span className="table-cell text-right">
-            {getPercentage(Recommended)}
-          </span>
-        </li>
-        <li className="table-row">
-          <span
-            className="table-cell text-right"
-            role="img"
-            aria-label="not recommended"
-          >
-            😓:
-          </span>
-          <span className="table-cell px-1 text-right">{notRec}</span>
-          <span className="table-cell text-right">{getPercentage(notRec)}</span>
-        </li>
-      </ul>
-    </div>
-  );
 };
 
 export const PlayerCountChart = ({
