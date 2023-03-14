c693c96695a1f20d3d0e834293c47aa16c70ab55
David Horm <davidhorm@gmail.com>
2023-03-13 22:12:10 -0700

    fix: SimpleBoardGame export path

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,3 @@
 export type { BoardGame } from "./services/filter-sort-services/useCollectionFilters";
 
-export type { SimpleBoardGame } from "./components/SearchFilterForm/BggCollection/hooks/useGetCollectionQuery.utils";
+export type { SimpleBoardGame } from "./components/BggCollection/hooks/useGetCollectionQuery.utils";
