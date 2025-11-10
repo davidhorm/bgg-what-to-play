32e0e070f0e52392827bbf9371d63ea66e34d1e2
David Horm <davidhorm@gmail.com>
2025-11-09 20:47:12 -0800

    fix: hide message

--- app/src/components/BggCollection/GameCard/PlayerCountChart/MaybeNoDataAvailable.tsx
+++ app/src/components/BggCollection/GameCard/PlayerCountChart/MaybeNoDataAvailable.tsx
@@ -16,8 +16,8 @@ export const MaybeNoDataAvailable = (
     return null;
   }
 
-  const h = isNaN(height) ? 2 : height;
-  const w = isNaN(width) ? 2 : width;
+  const h = isNaN(height) ? 0 : height;
+  const w = isNaN(width) ? 0 : width;
 
   return (
     <text dy={h / 2} dx={w / 2} fontSize="0.875rem" textAnchor="middle">
